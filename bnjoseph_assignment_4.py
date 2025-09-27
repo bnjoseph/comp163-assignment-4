7e7e1af9325a8b07118bf0e09de5b14765b5f267
bnjoseph <bnjoseph@aggies.ncat.edu>
2025-09-26 20:30:11 -0400

    Add files via upload
    Pass Test Case 2: Course planning with if/elif/else and comparison operators

--- bnjoseph_assignment_4.py
+++ bnjoseph_assignment_4.py
@@ -23,3 +23,22 @@ elif choice == "C":
     study_hours = 18
 else:
     print("Input is invalid")
+
+#2
+#if statements to check gpa and effect study_hours, stress_level (right now), and social_points
+if current_gpa < 2.1 and current_gpa >= 1.0:
+    study_hours = study_hours * 1.5
+    stress_level += study_hours * 10
+    social_points += 3
+elif current_gpa < 3.5 and current_gpa >= 2.1:
+    study_hours = study_hours * 1.25
+    stress_level += study_hours * 10
+    social_points += 2
+else:
+    study_hours = study_hours * 1
+    stress_level += study_hours * 10
+    social_points += 1
+#display values
+print("Study Hours:", study_hours)
+print("Stress Level:", stress_level)
+print("Social Points:", social_points)
